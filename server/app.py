1b04f9f27f37d57b4c477a4a1ffbe5a69cb50e6d
Martin <mmaina290@gmail.com>
2024-03-01 04:14:55 +0300

    progress

--- server/app.py
+++ server/app.py
@@ -19,15 +19,23 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+     animal = Animal.query.get_or_404(id)
+     response_data = f"<ul>ID: {animal.id}</li><li>Name: {animal.name}</li><li>Species:{animal.species}</li><li>Zookeper: {animal.zookeeper.name}</li><li>Enclosure: {animal.enclosure.environment}</li></ul>"
+     return make_response(response_data, 200)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get_or_404(id)
+    animals_list = '<li>' + '</li><li>'.join([animal.name for animal in zookeeper.animals]) + '</li>'
+    response_data = f"<ul><li>Name: {zookeeper.name}</li><li>Birthday: {str(zookeeper.birthday)}</li><li>{len(zookeeper.animals)} Animal:<ul>{animals_list}</ul></li></ul>"
+    return make_response(response_data, 200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get_or_404(id)
+    animals_list = '<li>' + '</li><li>'.join([animal.name for animal in enclosure.animals]) + '</li>'
+    response_data = f"<ul><li>Environment: {enclosure.environment} Open to Visitors: {'open to visitors' if enclosure.open_to_visitors else 'not open to visitors'}</li><li>{len(enclosure.animals)} Animals:<ul>{animals_list}</ul></li></ul>"
+    return make_response(response_data, 200)
 
 
 if __name__ == '__main__':
